8ebd032e4f434839dd8af04e21689621268cf634
JhonyDomingos <jhonatandomingossilva@oulook.com.br>
2023-11-24 01:14:14 -0300

    att

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 # Definindo a variável de ambiente
 os.environ['FLASK_DEBUG'] = 'True'
 
-# Configurando o modo de depuração com base na variável de ambiente
+# Configurando o modo de depuração com base na variável de ambiente!
 app.debug = os.environ.get('FLASK_DEBUG') == 'True'
 
 @app.route('/')
